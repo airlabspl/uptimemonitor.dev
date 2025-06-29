92171f8e219a20e74c9e13d99921ae50bb7363e1
Krzysztof <krzysztof@engineer.com>
2025-06-29 14:03:08 +0200

    tweak rollup output

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,6 +5,31 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          table: ['@tanstack/react-table'],
+          recharts: ['recharts'],
+          radix: [
+              "@radix-ui/react-avatar",
+              "@radix-ui/react-checkbox",
+              "@radix-ui/react-dialog",
+              "@radix-ui/react-dropdown-menu",
+              "@radix-ui/react-label",
+              "@radix-ui/react-select",
+              "@radix-ui/react-separator",
+              "@radix-ui/react-slot",
+              "@radix-ui/react-tabs",
+              "@radix-ui/react-toggle",
+              "@radix-ui/react-toggle-group",
+              "@radix-ui/react-tooltip",
+          ]
+        }
+      }
+    }
+  },  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
