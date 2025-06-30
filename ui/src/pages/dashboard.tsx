4c72a6e083871701328c84322daa196f9ebe632c
Krzysztof <krzysztof@engineer.com>
2025-06-30 17:27:05 +0200

    visual tweaks

--- ui/src/pages/dashboard.tsx
+++ ui/src/pages/dashboard.tsx
@@ -1,4 +1,3 @@
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
 import data from "@/app/dashboard/data.json"
@@ -7,9 +6,6 @@ export default function Dashboard() {
     return (
         <>
             <SectionCards />
-            <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
-            </div>
             <DataTable data={data} />
         </>
     )
